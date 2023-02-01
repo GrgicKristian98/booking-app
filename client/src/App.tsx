ef5a4612d0db4eb618fa9bff7d27e84f8852b4b3
Kristian Grgić <kristigrgic5@gmail.com>
2023-02-01 20:50:38 +0100

    tailwind init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='App'>
+      <div className='p-6 max-w-md mx-auto bg-white rounded-xl shadow-md items-center space-x-4'>
+        <h2 className='text-2xl font-bold'>Create a session</h2>
+        <div className='mt-8 max-w-md'>
+          <div className='grid grid-cols-1 gap-6'>
+            <label className='block'>
+              <span className='text-gray-700'>From</span>
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
